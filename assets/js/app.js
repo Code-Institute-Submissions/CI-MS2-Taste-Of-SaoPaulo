b2754fab8db68627f53c43b3beafd2b415c40a48
roliveira81 <roberto.oliveira.ti@gmail.com>
2021-02-08 08:49:23 +0000

    Fix header and footer color

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ menu_item.forEach((item) => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#05303e';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
